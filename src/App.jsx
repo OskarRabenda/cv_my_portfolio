1be2feb3b7542e6b348ff2edcfc41874d34622d4
OskarRabenda <o.a.rabenda@student.tue.nl>
2026-06-15 22:54:38 +0200

    Make neural noise background red

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './sections/Hero.jsx';
 export default function App() {
   return (
     <>
-      <NeuralNoise color={[0.35, 0.55, 1.0]} opacity={0.85} />
+      <NeuralNoise color={[1.0, 0.1, 0.15]} opacity={0.85} />
       <Hero />
     </>
   );
