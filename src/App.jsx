9648076034d7ef95d38f94ec54cca1c866145a05
OskarRabenda <o.a.rabenda@student.tue.nl>
2026-06-16 14:44:56 +0200

    Add home button on non-home pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './theme/ThemeProvider.jsx';
 import { GradientBackground } from './components/ui/GradientBackground.jsx';
 import { NavBar } from './components/ui/NavBar.jsx';
+import { HomeButton } from './components/ui/HomeButton.jsx';
 import { ThemeSwitcher } from './components/ui/ThemeSwitcher.jsx';
 import Hero from './sections/Hero.jsx';
 import Contact from './pages/Contact.jsx';
@@ -11,6 +12,7 @@ export default function App() {
     <ThemeProvider>
       <BrowserRouter>
         <NavBar />
+        <HomeButton />
         <Routes>
           <Route
             path="/"
